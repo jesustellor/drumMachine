86f8b3b5c8264ec5df0a921ff2375847fb279448
jesustellor <jesustellor@gmail.com>
2022-12-06 10:28:58 -0800

    working on drum machine functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+let handleButton = (event) => {
+  var x = event.key;
+  if(x === 'q'){
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3");
+    audio.play();
+  }else if(x === 'w'){
+    let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+    audioW.play()
+
+  }
+}
+let handleClick = (event) => {
+  let value = event.target.innerText;
+  console.log(event);
+    if(value === 'Q'){
+      let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3");
+      audio.play();
+    }else if(value === 'W'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
+    }  
+}
+
+useEffect(() => {
+  window.addEventListener('keydown', handleButton);
+  // return cleanup funtion to remove the handler after use
+  return () => {
+      window.removeEventListener('keydown', handleButton);
+  } 
+}, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button id='buttonQ' onClick={handleClick}>Q</button>
+          <button id='buttonW' onClick={handleClick}>W</button>
+
       </header>
     </div>
   );

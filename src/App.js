9b22da76f6859aefb774f96662ef94e53272d793
jesustellor <jesustellor@gmail.com>
2022-12-06 18:01:41 -0800

    working on switch

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  
 let handleButton = (event) => {
   var x = event.key;
   if(x === 'q'){
     let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3");
     audio.play();
   }else if(x === 'w'){
-    let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
-    audioW.play()
-
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+    audio.play()
+  }else if(x === 'e'){   
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3");
+    audio.play()
+  }else if(x === 'a'){
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3");
+    audio.play()    
+  }else if(x === 's'){
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3");
+    audio.play()  
+  }else if(x === 'd'){
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3");
+    audio.play()  
+  }else if(x === 'z'){
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3");
+    audio.play()  
+  }else if(x === 'x'){
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3");
+    audio.play()  
+  }else if(x === 'c'){   
+    let audio = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3");
+    audio.play()  
   }
+  console.log(x);
+
 }
+
 let handleClick = (event) => {
   let value = event.target.innerText;
   console.log(event);
@@ -22,7 +46,39 @@ let handleClick = (event) => {
     }else if(value === 'W'){
       let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
       audioW.play()
+    }else if(value === 'E'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
+    }else if(value === 'A'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
+    }else if(value === 'S'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
+    }else if(value === 'D'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
+    }else if(value === 'Z'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
+    }else if(value === 'X'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
+    }else if(value === 'C'){
+      let audioW = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3");
+      audioW.play()
     }  
+}
+
+let handleSwitch = () => {
+  let switched = document.getElementById('switch');
+  if(switched.checked){
+    handleButton();
+    console.log("Switch is on");
+  }else if(!switched.checked){
+    handleClick()
+    console.log("Switch is Off");
+  }
 }
 
 useEffect(() => {
@@ -37,8 +93,23 @@ useEffect(() => {
   return (
     <div className="App">
       <header className="App-header">
-          <button id='buttonQ' onClick={handleClick}>Q</button>
-          <button id='buttonW' onClick={handleClick}>W</button>
+          <button onClick={handleSwitch}>Q</button>
+          <button onClick={handleSwitch}>W</button>
+          <button onClick={handleSwitch}>E</button>
+          <button onClick={handleSwitch}>A</button>
+          <button onClick={handleSwitch}>S</button>
+          <button onClick={handleSwitch}>D</button>
+          <button onClick={handleSwitch}>Z</button>
+          <button onClick={handleSwitch}>X</button>
+          <button onClick={handleSwitch}>C</button>
+
+          <label className="switch">
+            <input id='switch' onClick={handleSwitch} type="checkbox"/>
+              <span className="slider round"></span>
+          </label>
+
+
+
 
       </header>
     </div>
